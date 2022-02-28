79629d42deb4c255526e593bdcbafebcc9935829
fullofhell <jacksummers715@gmail.com>
2022-02-28 12:08:20 -0500

    nuke duplicate padding

--- config/common.mk
+++ config/common.mk
@@ -183,13 +183,13 @@ PRODUCT_PRODUCT_PROPERTIES += \
 	persist.sys.disable_rescue=true
 
 # Gboard side padding
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.com.google.ime.kb_pad_port_l=4 \
-    ro.com.google.ime.kb_pad_port_r=4 \
-    ro.com.google.ime.kb_pad_land_l=64 \
-    ro.com.google.ime.kb_pad_land_r=64 \
-    ro.com.google.ime.kb_pad_port_b=0 \
-    ro.com.google.ime.kb_pad_land_b=0
+#PRODUCT_PRODUCT_PROPERTIES += \
+#    ro.com.google.ime.kb_pad_port_l=4 \
+#    ro.com.google.ime.kb_pad_port_r=4 \
+#    ro.com.google.ime.kb_pad_land_l=64 \
+#    ro.com.google.ime.kb_pad_land_r=64 \
+#    ro.com.google.ime.kb_pad_port_b=0 \
+#    ro.com.google.ime.kb_pad_land_b=0
 
 # Blur
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
