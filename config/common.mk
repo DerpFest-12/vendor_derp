Octavi-OS/platform_vendor_octavi

vendor: Add columbus overlay sysconfig

config/common.mk
@@ -84,6 +84,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     vendor/octavi/prebuilt/common/etc/sysconfig/game_overlay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/game_overlay.xml
 
+# Quick Tap
+PRODUCT_COPY_FILES += \
+    vendor/octavi/prebuilt/common/etc/sysconfig/quick_tap.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/quick_tap.xml
+
 # Gapps
 ifeq ($(WITH_GAPPS),true)
 $(call inherit-product, vendor/gms/products/gms.mk)

DerpFest-12/vendor_derp

config/common.mk
@@ -70,6 +70,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=enforce
 
+# Quick Tap
+ifeq ($(TARGET_SUPPORTS_QUICK_TAP), true)
+PRODUCT_COPY_FILES += \
+    vendor/derp/prebuilt/common/etc/sysconfig/quick_tap.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/quick_tap.xml
+endif
+
 # Include AOSP audio files
 include vendor/derp/config/aosp_audio.mk
 